81e40aee3423a1d0a6041a1a8a96bb7b7a459d25
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-29 09:58:36 +0100

    last updates in postman collections; adjust routes in auth-service to adapt to Jose's nomenclature; change gateway accordingly

--- auth-service/src/main/java/com/barrisense/backend/auth/controller/ProtectedAuthController.java
+++ auth-service/src/main/java/com/barrisense/backend/auth/controller/ProtectedAuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth/protected")
+@RequestMapping("/api/auth/protected")
 @RequiredArgsConstructor
 public class ProtectedAuthController {
 
--- auth-service/src/main/java/com/barrisense/backend/auth/controller/PublicAuthController.java
+++ auth-service/src/main/java/com/barrisense/backend/auth/controller/PublicAuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class PublicAuthController {
 
