0b1d69ee4f9b0c5e527292ae924bdbce0b18cd76
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-29 09:48:48 +0100

    removed public from public paths in auth controller

--- auth-service/src/main/java/com/barrisense/backend/auth/controller/PublicAuthController.java
+++ auth-service/src/main/java/com/barrisense/backend/auth/controller/PublicAuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth/public")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class PublicAuthController {
 
--- feedback-service/src/main/java/com/barriSenseBack/barrisense_feedback_api/dto/UserDTO.java
+++
@@ -1,4 +0,0 @@
-package com.barriSenseBack.barrisense_feedback_api.dto;
-
-public record UserDTO(String password, String name, String email) {
-}
