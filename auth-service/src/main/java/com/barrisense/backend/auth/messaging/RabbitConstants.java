3b2564181fd0c7d6b9dcc70734eb3a60ece53145
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-10-30 17:43:17 +0100

    added RabbitConstants in auth and user service

---
+++ auth-service/src/main/java/com/barrisense/backend/auth/messaging/RabbitConstants.java
@@ -0,0 +1,11 @@
+package com.barrisense.backend.auth.messaging;
+
+public class RabbitConstants {
+    public static final String USER_EXCHANGE = "user.exchange";
+    public static final String USER_CREATED_QUEUE = "user.created.queue";
+    public static final String USER_CREATED_ROUTING_KEY = "user.created";
+
+    private RabbitConstants() {
+    }
+
+}
